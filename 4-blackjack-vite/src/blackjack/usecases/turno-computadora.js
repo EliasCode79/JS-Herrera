8a1c9b468ab38d1057f42cc8434afdffd4746de5
EliasCode79 <eliasfranco2379@gmail.com>
2023-04-28 20:52:31 -0400

    refactor: :recycle: Creamos funciones para reutilizar ciertas funcionalidades

---
+++ 4-blackjack-vite/src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,41 @@
+import { crearCartaHtml, pedirCarta, valorCarta } from './';
+// turno de la computadora
+
+/**
+ * Turno de la computadora
+ * @param {Number} puntosMinimos puntos minimos q la computadora necesita para ganar.
+ * @param {HTMLElement} puntosHTML elemento html para mostrar los puntos.
+ * @param {HTMLElement} divCartasComputadora elemento html para mostrar las cartas.
+ * @param {Array<string>} deck
+ */
+export const turnoComputadora = (puntosMinimos, puntosHTML, divCartasComputadora, deck = []) => {
+	if (!puntosMinimos) throw new Error('Puntos minimos son necesarios');
+	if (!puntosHTML) throw new Error('Argumentos puntosHTML son necesarios');
+	let puntosComputadora = 0;
+	do {
+		const carta = pedirCarta(deck);
+
+		puntosComputadora = puntosComputadora + valorCarta(carta);
+		puntosHTML.innerText = puntosComputadora;
+
+		// <img class="carta" src="assets/cartas/2C.png">
+		const imgCarta = crearCartaHtml(carta);
+		divCartasComputadora.append(imgCarta);
+
+		if (puntosMinimos > 21) {
+			break;
+		}
+	} while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+
+	setTimeout(() => {
+		if (puntosComputadora === puntosMinimos) {
+			alert('Nadie gana :(');
+		} else if (puntosMinimos > 21) {
+			alert('Computadora gana');
+		} else if (puntosComputadora > 21) {
+			alert('Jugador Gana');
+		} else {
+			alert('Computadora Gana');
+		}
+	}, 100);
+};
