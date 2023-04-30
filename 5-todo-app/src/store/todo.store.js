2af3c29cbe654a3308b64e045e58157782a38a03
EliasCode79 <eliasfranco2379@gmail.com>
2023-04-30 16:37:05 -0400

    feat: :sparkles: Listado de todos los to-dos

--- 5-todo-app/src/store/todo.store.js
+++ 5-todo-app/src/store/todo.store.js
@@ -7,7 +7,13 @@ const Filters = {
 };
 
 const state = {
-	todos: [new Todo('Piedra del infinito'), new Todo('Piedra del alma'), new Todo('Piedra del destruccion')],
+	todos: [
+		new Todo('Piedra del infinito'),
+		new Todo('Piedra del alma'),
+		new Todo('Piedra del destruccion'),
+		new Todo('Piedra del realidad'),
+		new Todo('Piedra del tiempo'),
+	],
 	filter: Filters.All,
 };
 
--- 5-todo-app/src/todos/app.js
+++ 5-todo-app/src/todos/app.js
@@ -1,14 +1,26 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from './casos-usos';
+
+const elementIDs = {
+	TodoList: '.todo-list',
+};
 
 /**
  *
  * @param {String} elementId un string
  */
 export const App = (elementId) => {
+	const displayTodos = (elementId) => {
+		const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+		renderTodos(elementIDs.TodoList, todos);
+	};
+
 	// Funciona autoinvocada, Cuando App() se llama
 	(() => {
 		const app = document.createElement('div');
 		app.innerHTML = html;
 		document.querySelector(elementId).append(app);
+		displayTodos();
 	})();
 };
